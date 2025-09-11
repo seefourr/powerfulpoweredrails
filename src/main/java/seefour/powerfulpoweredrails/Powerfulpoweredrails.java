78eb21e207da8bd2d48650a0f4e977e48b4c3686
seefourr <106939690+seefourr@users.noreply.github.com>
2025-09-12 04:07:43 +1000

    comment

--- src/main/java/seefour/powerfulpoweredrails/Powerfulpoweredrails.java
+++ src/main/java/seefour/powerfulpoweredrails/Powerfulpoweredrails.java
@@ -7,7 +7,8 @@ import org.slf4j.LoggerFactory;
 public class Powerfulpoweredrails implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("powerfulpoweredrails");
 
-    public static double ACCELERATION = 0.24; // our desired value for minecart acceleration in meters per tick per tick. If you want to change the acceleration this is the value you want.
+    public static double ACCELERATION = 0.24; // our desired value for minecart acceleration in meters per tick per tick (meters per tick the speed of the minecart increases for every tick it is over a powered rail). 
+    // If you want to change the acceleration this is the value you want.
 
     @Override
     public void onInitialize() {
